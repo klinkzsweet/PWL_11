bdd3acfa91b14952bda0d7c5decec7e560138559
klinkzsweet <klinkzsweet@gmail.com>
2022-04-29 16:44:21 +0700

    praktikum 1

---
+++ app/Http/Requests/ApiRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Traits\ApiResponse;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Response;
+
+abstract class ApiRequest extends FormRequest
+{
+    use ApiResponse;
+    abstract public function rules();
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException($this->apiError(
+            $validator->errors(),
+            Response::HTTP_UNPROCESSABLE_ENTITY,
+        ));
+    }
+    protected function failedAuthorization()
+    {
+        throw new HttpResponseException($this->apiError(
+            null,
+            Response::HTTP_UNAUTHORIZED,
+        ));
+    }
+}
